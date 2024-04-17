636fe0edc577c98e85ea27e08885016cdbb4d29c
jakobniinja <dansaren124@outlook.com>
2024-04-17 10:04:08 +0200

    Wizard game now complete

--- src/main/java/anette/godtland/WizardOfYesNo.java
+++ src/main/java/anette/godtland/WizardOfYesNo.java
@@ -1,16 +1,46 @@
 package anette.godtland;
 
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 public class WizardOfYesNo extends JFrame {
 
-  public static final String ANSWER = "NO";
+  private static final Long serialVersionUID = 1L;
+
+  protected static final String[] ANSWERS = {
+      "Yes.",
+      "Go for it!",
+      "For sure!",
+      "I would say yes.",
+      "Most likely yes.",
+      "No.",
+      "I wouldn't.",
+      "In my opinion, no.",
+      "Definitely not!",
+      "Probably not.",
+      "It is very doubtful."
+  };
 
   public WizardOfYesNo() {
+    Random random = new Random();
+    int numberOfAnswers = ANSWERS.length;
+    int pick = random.nextInt(numberOfAnswers);
+    String answer = ANSWERS[pick];
+
     JLabel label = new JLabel();
-    label.setText(ANSWER);
+    label.setText(answer);
+    Font font = new Font(Font.SERIF, Font.BOLD, 32);
+    label.setFont(font);
+    label.setHorizontalAlignment(SwingConstants.CENTER);
     add(label);
+    setTitle("Wizard Yes/No");
+    setResizable(false);
+    pack();
     setSize(400, 200);
     setLocationRelativeTo(null);
 
@@ -19,6 +49,12 @@ public class WizardOfYesNo extends JFrame {
   }
 
   public static void main(String[] args) {
-    new WizardOfYesNo();
+    try {
+      String className = UIManager.getCrossPlatformLookAndFeelClassName();
+      UIManager.setLookAndFeel(className);
+
+      EventQueue.invokeLater(WizardOfYesNo::new);
+    } catch (Exception e) {
+    }
   }
 }
